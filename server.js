b0695634a4f821dffd8792a4d340d39096534c50
N13L5A97 <niels.aling@hotmail.com>
2023-02-20 16:57:19 +0100

    add eslint files

--- server.js
+++ server.js
@@ -5,19 +5,15 @@ const port = 3000
 
 app.use(express.static('static'))
 
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-
-app.get('/home/:user/' , (req, res) => {
+app.get('/home/:user/', (req, res) => {
   console.log('Input from' + req.params.user)
   res.send('<img src="/images/spiderman.png" width="150"> <h1>Hello ' + req.params.user + '</h1>')
 })
 
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
