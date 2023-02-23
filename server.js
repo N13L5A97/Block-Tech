85aa034cd90dea0ba29b74be5eba568b4467fb65
N13L5A97 <niels.aling@hotmail.com>
2023-02-23 13:07:37 +0100

    add a lot of files

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 const express = require('express')
-
 const app = express()
-const port = 3000
-
+const port = 8080
 app.use(express.static('static'))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// set the view engine to ejs
+app.set('view engine', 'ejs')
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function (req, res) {
+  res.render('pages/index')
 })
 
-app.get('/home/:user/', (req, res) => {
-  console.log('Input from' + req.params.user)
-  res.send('<img src="/images/spiderman.png" width="150"> <h1>Hello ' + req.params.user + '</h1>')
+// All Maps page
+app.get('/all', function (req, res) {
+  res.render('pages/all')
+  let games = [
+    { name: 'Spider-man', img: '/images/spiderman.png' }, { name: 'sci-fi', img: '/images/sci-fi.jpeg' }
+  ]
+  var tagline = 'The Amazing Spiderman'
+
+  res.render('pages/all', {
+    games: games,
+    tagline: tagline
+  })
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(port)
+console.log('Server is listening on port 8080')
