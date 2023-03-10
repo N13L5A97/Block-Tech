4d699dc9ca48db18d9209a4078dcd1399f656704
N13L5A97 <niels.aling@hotmail.com>
2023-03-10 09:01:20 +0100

    error page and result page

--- server.js
+++ server.js
@@ -2,25 +2,38 @@ const express = require('express')
 const app = express()
 const port = 8080
 
+// gebruik ejs als templating engine
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+// static folder
+app.use(express.static('static'))
+
+// enable parsing of http request body
+app.use(express.urlencoded({ extended: true }))
+
+// zet geheime variabelen in .env bestand
 require('dotenv').config()
 
+// connect to MongoDB
 const { MongoClient } = require('mongodb')
-
 const uri = process.env.MONGODB_URI
 const client = new MongoClient(uri)
+
+// maak variabelen voor de database en de collectie
 const db = client.db('mapsplore')
 const coll = db.collection('maps')
 
-app.set('view engine', 'ejs')
-app.set('views', 'views')
-app.use(express.static('static'))
+client.connect(err => {
+  if (err) { throw err }
+})
 
-app.use(express.urlencoded({ extended: true }))
-
+// home page
 app.get('/', function (req, res) {
   res.render('pages/index')
 })
 
+// all maps page
 app.get('/all', async (req, res) => {
   try {
     const maps = await coll.find().toArray()
@@ -31,9 +44,10 @@ app.get('/all', async (req, res) => {
   }
 })
 
-app.post('/results', async function (req, res) {
+// result page
+app.post('/results', async (req, res) => {
   const { year, season, envoirement, size } = req.body
-
+  // function to check the input and store it in an array
   try {
     const maps = await coll.find({
       year,
@@ -43,7 +57,7 @@ app.post('/results', async function (req, res) {
     }).toArray()
 
     if (maps.length === 0) {
-      res.render('pages/no-matches')
+      res.render('pages/404')
     } else {
       res.render('pages/results', { maps })
     }
@@ -53,5 +67,16 @@ app.post('/results', async function (req, res) {
   }
 })
 
+// 404 error page
+app.use(function (req, res, next) {
+  res.status(404).render('pages/404')
+})
+
 app.listen(port)
 console.log('Server is listening on port 8080')
+
+// gebruik crud 2x (create, read, update, delete)
+// find = read
+// insert = create
+// update = update
+// delete = delete
