59fe5726596fd58d5e422060fa7bc46afcb3dd99
N13L5A97 <niels.aling@hotmail.com>
2023-03-14 15:43:46 +0100

    add comments

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,9 +1,11 @@
-// const randomButton = document.querySelector('#randomButton')
+// find the tempature in the html
 const tempString = document.querySelector('#temp')
 console.log(tempString)
 
+// make string into number
 const temp = parseInt(tempString.innerHTML)
 
+// check if temp is higher than 0 and give user alert
 if (temp > 0) {
   console.log('Het is lekker weer, ga naar buiten!')
   alert('Het is lekker weer, ga naar buiten!')
